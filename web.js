311774ee68e478c42d8192fed12a6795cf6a6b55
Alex Grintsvayg <git@grin.io>
2019-08-23 10:11:39 -0400

    make it work for our case

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 
-var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var newDomain = process.env.NEW_DOMAIN || 'example.com';
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
 
 app.get('*', function(request, response) {
-  response.redirect(redirectStatus, newBaseURL + request.url);
+  const domain = request.hostname.split('.').slice(0,-2).concat(newDomain).join('.');
+  response.redirect(redirectStatus, 'http' + (request.secure?'s':'') + "://" + domain + request.url);
 });
 
 app.listen(port, function() {
